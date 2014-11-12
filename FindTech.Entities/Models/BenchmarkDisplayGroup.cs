4bc63f1d27fd609834464803546789be97baf02a
troyuit@gmail.com <troyuit@gmail.com@4293a4ba-4aa9-1431-8d18-6718c6a779e9>
2014-11-12 04:11:50 +0000

    create Benchmark table

---
+++ FindTech.Entities/Models/BenchmarkDisplayGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Pattern.Ef6;
+
+namespace FindTech.Entities.Models
+{
+    public class BenchmarkDisplayGroup : Entity
+    {
+        public int BenchmarkDisplayGroupId { get; set; }
+        public string BenchmarkDisplayGroupName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FindTech.Entities/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using FindTech.Entities.Models.Enums;
+using Repository.Pattern.Ef6;
+
+namespace FindTech.Entities.Models
+{
+    public class Device : Entity
+    {
+        public int DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        public string Avatar { get; set; }
+        public string Description { get; set; }
+        public MarketStatus MarketStatus { get; set; }
+        public int ViewCount { get; set; }
+    }
+}
