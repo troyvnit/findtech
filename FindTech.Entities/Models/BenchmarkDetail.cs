13def241b588e179f31e35a4a71d337989b278ac
troyuit@gmail.com <troyuit@gmail.com@4293a4ba-4aa9-1431-8d18-6718c6a779e9>
2014-12-11 09:14:47 +0000

    Update BenchmarkDetail table

--- FindTech.Entities/Models/BenchmarkDetail.cs
+++ FindTech.Entities/Models/BenchmarkDetail.cs
@@ -9,6 +9,8 @@ namespace FindTech.Entities.Models
         public string Unit { get; set; }
         public string Value { get; set; }
         public string WayComparison { get; set; }
+        public string Rate { get; set; }
+        public string Description { get; set; }
         public int BenchmarkId { get; set; }
         public Benchmark Benchmark { get; set; }
         public int DeviceId { get; set; }
