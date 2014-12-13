75d304d7db9d6ba84547c4c4e8f1d7459a9fdc2a
votrannhatminh1508@gmail.com <votrannhatminh1508@gmail.com@4293a4ba-4aa9-1431-8d18-6718c6a779e9>
2014-12-13 03:30:16 +0000

    Minh : ArticleBO module Thêm 2 dropdownlist cho danh mục, nguồn

--- trunk/FindTech.Web/Areas/BO/Models/ArticleBOViewModel.cs
+++ trunk/FindTech.Web/Areas/BO/Models/ArticleBOViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using FindTech.Entities.Models.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace FindTech.Web.Areas.BO.Models
 {
@@ -15,7 +17,11 @@ namespace FindTech.Web.Areas.BO.Models
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public string SourceId { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public BoxSize BoxSize { get; set; }
+       
+        [JsonConverter(typeof(StringEnumConverter))]
         public ArticleType ArticleType { get; set; }
         public bool? IsActived { get; set; }
         public bool? IsDeleted { get; set; }
