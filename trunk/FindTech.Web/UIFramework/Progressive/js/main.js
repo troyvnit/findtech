d56bc7caa2729eb7889bbf6be5f6fadf554339d0
troyuit@gmail.com <troyuit@gmail.com@4293a4ba-4aa9-1431-8d18-6718c6a779e9>
2015-01-16 14:56:09 +0000

    Apply HTML & CSS for Homepage Apply HTML & CSS for Article Detail

--- trunk/FindTech.Web/UIFramework/Progressive/js/main.js
+++ trunk/FindTech.Web/UIFramework/Progressive/js/main.js
@@ -2085,13 +2085,13 @@ jQuery(document).ready(function(){
   
   //Facebook
   if ($('.facebook-widget').length) {
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_EN/all.js#xfbml=1";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+      (function (d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=136048103211226&version=v2.0";
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
   }
   
   //Twitter
