a995f84c5aa42577ff68993e9d3b2ed671181d82
pvtho.uit@gmail.com <pvtho.uit@gmail.com@4293a4ba-4aa9-1431-8d18-6718c6a779e9>
2015-02-10 16:30:55 +0000

    fix htmldecode

---
+++ FindTech.Services/CommentService.cs
@@ -0,0 +1,22 @@
+﻿using FindTech.Entities.Models;
+using Repository.Pattern.Repositories;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTech.Services
+{
+    public interface ICommentService : IService<Comment>
+    {
+    }
+    public class CommentService : Service<Comment>, ICommentService
+    {
+        public CommentService(IRepositoryAsync<Comment> commentRepository)
+            : base(commentRepository)
+        {
+        }
+    }
+}
--- FindTech.Web/Controllers/CommentController.cs
+++ FindTech.Web/Controllers/CommentController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using FindTech.Entities.Models;
+using FindTech.Services;
+using FindTech.Web.Models;
+using Newtonsoft.Json;
+using Repository.Pattern.UnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +14,14 @@ namespace FindTech.Web.Controllers
 {
     public class CommentController : Controller
     {
+        private ICommentService commentService { get; set; }
+        private IUnitOfWorkAsync unitOfWork { get; set; }
+
+        public CommentController(ICommentService commentService, IUnitOfWorkAsync unitOfWork)
+        {
+            this.commentService = commentService;
+            this.unitOfWork = unitOfWork;
+        }
         // GET: Comment
         public ActionResult Index()
         {
@@ -21,9 +35,15 @@ namespace FindTech.Web.Controllers
             return Json(false);
         }
 
-        public ActionResult Save(string comment)
+        public ActionResult Create(string model)
         {
-            return Json(false);
+            var commentViewModel = JsonConvert.DeserializeObject<CommentModel>(model);
+                var comment = Mapper.Map<Comment>(commentViewModel);
+                commentService.Insert(comment);
+                unitOfWork.SaveChanges();
+                //commentViewModel.Add(Mapper.Map<BrandBOViewModel>(brand));
+            //return Json(brandBOViewModels, JsonRequestBehavior.AllowGet);
+                return Json(false);
         }
         public ActionResult Update(string comment)
         {
