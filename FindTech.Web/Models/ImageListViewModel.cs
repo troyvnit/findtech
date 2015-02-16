70f8969a177b7ea5fb1967d28d7cb17645f9ce7a
troyuit@gmail.com <troyuit@gmail.com@4293a4ba-4aa9-1431-8d18-6718c6a779e9>
2015-02-16 10:13:03 +0000

    Apply images gallery for content section with JssorSlider

---
+++ FindTech.Web/Models/ImageListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FindTech.Entities.Models;
+
+namespace FindTech.Web.Models
+{
+    public class ImageListViewModel
+    {
+        public IEnumerable<Image> Images { get; set; } 
+    }
+}
